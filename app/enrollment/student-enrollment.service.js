1130b0d9c28dd9bbf62968e615dccbb727cf8ba7
Slobodanka Stojanova <slobodanka.trajkova@gmail.com>
2016-07-26 14:01:37 +0200

    Create studentInfo Directive,  Create Enrollment module

--- app/enrollment/student-enrollment.service.js
+++ app/enrollment/student-enrollment.service.js
@@ -1,112 +1,111 @@
 (function (angular) {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('ump')
-    .factory('EnrollmentService', EnrollmentServiceFn);
+    angular
+      .module('ump')
+      .factory('EnrollmentService', EnrollmentServiceFn);
 
-  EnrollmentServiceFn.$inject = ['$localStorage'];
+    EnrollmentServiceFn.$inject = ['$localStorage'];
 
-  /* @ngInject */
-  function EnrollmentServiceFn($localStorage) {
-    return {
-      findStudent: findStudentFn,
-      addUpdateEnroll: addUpdateEnrollFn,
-      getAllEnrollments: getAllEnrollmentsFn,
-      getPaged: getPagedFn,
-      deleteEnroll: deleteEnrollFn,
-      addGrade: addGradeFn
-    };
-    function deleteEnrollFn(enroll) {
-      var index = $localStorage.listOfEnrollments.indexOf(enroll);
-      if (index !== -1) {
-        $localStorage.listOfEnrollments.splice(index, 1)
-      }
-      return $localStorage.listOfEnrollments;
-    }
-
-    function getPagedFn(start,
-                        number,
-                        predicateObject,
-                        sortPredicate,
-                        sortReverse) {
-
-      var filtered = predicateObject ?
-        $filter('filter')($localStorage.listOfEnrollments, predicateObject) :
-        $localStorage.listOfEnrollments;
-
-      if (sortPredicate) {
-        filtered = $filter('orderBy')(filtered, sortPredicate, sortReverse);
-
-      }
-
-      var result = filtered.slice(start, start + number);
-      var numberOfPages = Math.ceil(filtered.length / number);
-
+    /* @ngInject */
+    function EnrollmentServiceFn($localStorage) {
       return {
-        data: result,
-        numberOfPages: numberOfPages
+        findStudent: findStudentFn,
+        addUpdateEnroll: addUpdateEnrollFn,
+        getAllEnrollments: getAllEnrollmentsFn,
+        getPaged: getPagedFn,
+        deleteEnroll: deleteEnrollFn,
+        addGrade: addGradeFn
       };
-
-
-    }
-
-    function getAllEnrollmentsFn() {
-      return $localStorage.listOfEnrollments;
-
-    }
-
-    function findStudentFn(item) {
-      var result = null;
-      angular.forEach($localStorage.ListOfStudents, function (e) {
-        if (e.index === item) {
-          result = e;
+      function deleteEnrollFn(enroll) {
+        var index = $localStorage.listOfEnrollments.indexOf(enroll);
+        if (index !== -1) {
+          $localStorage.listOfEnrollments.splice(index, 1)
         }
-      });
-      return result;
-    }
-
-    function initStageFn() {
-      if ($localStorage.listOfEnrollments === null || $localStorage.listOfEnrollments === undefined)
-        $localStorage.listOfEnrollments = [];
-
-    }
-
-    function changeGradeFn(oldValue, newValue) {
-      oldValue.grade = newValue.grade;
-
-    }
-
-    function existingEnrollFn(enroll) {
-      var result = false;
-      angular.forEach($localStorage.listOfEnrollments, function (e) {
-        if (e.course === enroll.course && e.professor === enroll.professor)
-          result = true;
-      });
-      return result;
-
-    }
-
-    function addGradeFn(enroll, grade) {
-      enroll.grade = grade;
-    }
-
-
-    function addUpdateEnrollFn(item) {
-      initStageFn();
-      var enroll = item;
-      var check = existingEnrollFn(item);
-      if (check === false) {
-        $localStorage.listOfEnrollments.push(item);
+        return $localStorage.listOfEnrollments;
       }
 
-      return $localStorage.listOfEnrollments;
+      function getPagedFn(start,
+                          number,
+                          predicateObject,
+                          sortPredicate,
+                          sortReverse) {
+
+        var filtered = predicateObject ?
+          $filter('filter')($localStorage.listOfEnrollments, predicateObject) :
+          $localStorage.listOfEnrollments;
+
+        if (sortPredicate) {
+          filtered = $filter('orderBy')(filtered, sortPredicate, sortReverse);
+
+        }
+
+        var result = filtered.slice(start, start + number);
+        var numberOfPages = Math.ceil(filtered.length / number);
+
+        return {
+          data: result,
+          numberOfPages: numberOfPages
+        };
+
+
+      }
+
+      function getAllEnrollmentsFn() {
+        return $localStorage.listOfEnrollments;
+
+      }
+
+      function findStudentFn(item) {
+        var result = null;
+        angular.forEach($localStorage.ListOfStudents, function (e) {
+          if (e.index === item) {
+            result = e;
+          }
+        });
+        return result;
+      }
+
+      function initStageFn() {
+        if ($localStorage.listOfEnrollments === null || $localStorage.listOfEnrollments === undefined)
+          $localStorage.listOfEnrollments = [];
+
+      }
+
+
+      function existingEnrollFn(enroll) {
+        var result = false;
+        angular.forEach($localStorage.listOfEnrollments, function (e) {
+          if (e.course === enroll.course && e.professor === enroll.professor)
+            result = true;
+        });
+        return result;
+
+      }
+
+      function addGradeFn(enroll, grade) {
+        enroll.grade = grade;
+      }
+
+
+      function addUpdateEnrollFn(item) {
+        initStageFn();
+        var enroll = item;
+        var check = existingEnrollFn(item);
+        if (check === false) {
+          $localStorage.listOfEnrollments.push(item);
+        }
+
+        return $localStorage.listOfEnrollments;
+
+
+      }
 
 
     }
-
 
   }
-
-}(angular));
+  (angular)
+)
+;
 
